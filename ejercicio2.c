8fd89d9c7d13203d74e82012dfbf2256e6577dea
ibaxdan <daniel06alejandroba@gmail.com>
2024-06-02 17:48:01 -0500

    Update ejercicio2.c

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 int main()
 {
+    //definir los datos de entrada
     float totalcompra,valorfinal ;
+
+    //lectura de datos
     printf ("ingresar el total de la compra: ") ;
     scanf ("%f",&totalcompra) ; 
+
+    //calculo del valor final
     valorfinal = totalcompra - (totalcompra * 0.15) ;
+
+    //datos de salida
     printf ("el total de su compra fue %.2f y el valor con descuento es %.2f", totalcompra, valorfinal) ;
+    
     return 0 ;
 }
