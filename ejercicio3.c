d38a608cfc5b70d689a3ee6a7cd91629c039df01
ibaxdan <daniel06alejandroba@gmail.com>
2024-06-02 17:48:58 -0500

    Update ejercicio3.c

--- ejercicio3.c
+++ ejercicio3.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 int main()
 {
+    //definir los datos de entrada
     float base, altura, area, perimetro ;
+
+    //lectura de los datos de entrdada
     printf ("ingresar el valor de la altura") ;
     scanf ("%f",&altura) ;
+    
     printf ("ingresar el valor de la base") ;
     scanf ("%f",&base) ;
+
+    //calculo del perimetro y el area
     perimetro = 2 * (base + altura) ;
-    base = base * altura ;
-    printf ("el valor del  perimetro es %.2f y el valor del area es %.2f", perimetro, base) ;
+    area = base * altura ;
+
+    //datos de salida
+    printf ("el valor del  perimetro es %.2f y el valor del area es %.2f", perimetro, area) ;
     return 0 ;
 }
